bc84da073809769326f1d3bbd2c620aa211b9573
Ikuma Yamashita <me@ikuma.cloud>
2025-12-13 18:43:13 +0900

    fix: Update stack name from "CdkStack" to "ResearchAgent"

--- apps/cdk/app.py
+++ apps/cdk/app.py
@@ -9,7 +9,7 @@ from src.cdk_stack import CdkStack
 app = cdk.App()
 CdkStack(
     app,
-    "CdkStack",
+    "ResearchAgent",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
